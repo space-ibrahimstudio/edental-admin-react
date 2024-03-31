17b2ac6b42a3ec5978530fbde8149f0c67779a00
Maulana Malik Ibrahim <ibrahimcpersonal@gmail.com>
2024-04-01 01:23:00 +0700

    feat: add edit function for outlet CUD

--- src/sections/branch-list.jsx
+++ src/sections/branch-list.jsx
@@ -20,6 +20,7 @@ export const BranchList = ({ sectionId }) => {
   const { showNotifications } = useNotifications();
   // data state
   const [branchData, setBranchData] = useState([]);
+  const [selectedData, setSelectedData] = useState(null);
   const [filteredData, setFilteredData] = useState([]);
   // conditional context
   const [isDataShown, setIsDataShown] = useState(true);
@@ -29,8 +30,20 @@ export const BranchList = ({ sectionId }) => {
   const [isLoading, setIsLoading] = useState(false);
   // perform action state
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
   // input state
   const [inputData, setInputData] = useState({
+    region: "",
+    name: "",
+    address: "",
+    phone: "",
+    mainregion: "",
+    postcode: "",
+    cctrGroup: "",
+    cctr: "",
+    coordinate: "",
+  });
+  const [currentData, setCurrentData] = useState({
     region: "",
     name: "",
     address: "",
@@ -94,13 +107,14 @@ export const BranchList = ({ sectionId }) => {
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+    setInputData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
 
-    setInputData((prevState) => {
-      return { ...prevState, [name]: value };
-    });
-
-    setErrors((prevErrors) => {
-      return { ...prevErrors, [name]: "" };
+    setErrors({
+      ...errors,
+      [name]: "",
     });
   };
 
@@ -139,10 +153,18 @@ export const BranchList = ({ sectionId }) => {
 
         const offset = (currentPage - 1) * limit;
         const data = await fetchDataList(offset, limit, "viewoutlet");
-        setBranchData(data.data);
-        setFilteredData(data.data);
-        setTotalPages(data.TTLPage);
 
+        if (data && data.data && data.data.length > 0) {
+          setBranchData(data.data);
+          setFilteredData(data.data);
+          setTotalPages(data.TTLPage);
+          setIsDataShown(true);
+        } else {
+          setBranchData([]);
+          setFilteredData([]);
+          setTotalPages(0);
+          setIsDataShown(false);
+        }
         closeForm();
       } catch (error) {
         console.error("Error occurred during submit branch:", error);
@@ -156,13 +178,123 @@ export const BranchList = ({ sectionId }) => {
     }
   };
   // end add data function
+  // start edit/delete data function
+  const openEdit = (
+    id,
+    region,
+    name,
+    address,
+    phone,
+    mainregion,
+    postcode,
+    cctrGroup,
+    cctr,
+    coordinate
+  ) => {
+    setSelectedData(id);
+    setCurrentData({
+      region,
+      name,
+      address,
+      phone,
+      mainregion,
+      postcode,
+      cctrGroup,
+      cctr,
+      coordinate,
+    });
+    setIsEditOpen(true);
+  };
+
+  const closeEdit = () => {
+    cleanInput();
+    setIsEditOpen(false);
+    setSelectedData(null);
+  };
+
+  const handleInputEditChange = (e) => {
+    const { name, value } = e.target;
+    setErrors({
+      ...errors,
+      [name]: "",
+    });
+
+    setCurrentData({
+      ...currentData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmitEdit = async () => {
+    let hasError = false;
+    const newErrors = { ...errors };
+
+    for (const key in currentData) {
+      if (currentData[key].trim() === "") {
+        newErrors[key] = "Data ini tidak boleh kosong";
+        hasError = true;
+      } else {
+        newErrors[key] = "";
+      }
+    }
+
+    if (hasError) {
+      setErrors(newErrors);
+      return;
+    }
+
+    const confirmEdit = window.confirm(
+      "Apakah anda yakin untuk menyimpan perubahan?"
+    );
+
+    if (confirmEdit) {
+      try {
+        setIsLoading(true);
+        await handleCUDBranch(currentData, "edit", selectedData);
+        showNotifications(
+          "success",
+          "Selamat! Data Cabang berhasil diperbarui."
+        );
+
+        const offset = (currentPage - 1) * limit;
+        const data = await fetchDataList(offset, limit, "viewoutlet");
+
+        if (data && data.data && data.data.length > 0) {
+          setBranchData(data.data);
+          setFilteredData(data.data);
+          setTotalPages(data.TTLPage);
+          setIsDataShown(true);
+        } else {
+          setBranchData([]);
+          setFilteredData([]);
+          setTotalPages(0);
+          setIsDataShown(false);
+        }
+        closeEdit();
+      } catch (error) {
+        console.error("Error editing branch data:", error);
+        showNotifications(
+          "danger",
+          "Gagal menyimpan perubahan. Mohon periksa koneksi internet anda dan muat ulang halaman."
+        );
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+  // end edit/delete data function
   const tableHeadData = (
     <TableRow type="heading">
       <TableHeadValue type="num" value="NO" />
       <TableHeadValue value="Nama Cabang" />
       <TableHeadValue value="Alamat Cabang" />
-      <TableHeadValue value="Provinsi" />
-      <TableHeadValue value="Kontak" position="end" />
+      <TableHeadValue value="Main Region" />
+      <TableHeadValue value="Region" />
+      <TableHeadValue value="CCTR Group" />
+      <TableHeadValue value="CCTR" />
+      <TableHeadValue value="Telepon" />
+      <TableHeadValue value="Kode POS" />
+      <TableHeadValue value="Titik Koordinat" position="end" />
     </TableRow>
   );
 
@@ -173,9 +305,17 @@ export const BranchList = ({ sectionId }) => {
         const offset = (page - 1) * limit;
         const data = await fetchDataList(offset, limit, "viewoutlet");
 
-        setBranchData(data.data);
-        setFilteredData(data.data);
-        setTotalPages(data.TTLPage);
+        if (data && data.data && data.data.length > 0) {
+          setBranchData(data.data);
+          setFilteredData(data.data);
+          setTotalPages(data.TTLPage);
+          setIsDataShown(true);
+        } else {
+          setBranchData([]);
+          setFilteredData([]);
+          setTotalPages(0);
+          setIsDataShown(false);
+        }
       } catch (error) {
         console.error("Error fetching branch data:", error);
         showNotifications(
@@ -236,15 +376,37 @@ export const BranchList = ({ sectionId }) => {
         loading={isLoading}
       >
         {filteredData.map((branch, index) => (
-          <TableRow key={index} isEven={index % 2 === 0}>
+          <TableRow
+            key={index}
+            isEven={index % 2 === 0}
+            onClick={() =>
+              openEdit(
+                branch.idoutlet,
+                branch.outlet_region,
+                branch.outlet_name,
+                branch.outlet_address,
+                branch.outlet_phone,
+                branch.mainregion,
+                branch.postcode,
+                branch.cctr_group,
+                branch.cctr,
+                branch.coordinate
+              )
+            }
+          >
             <TableBodyValue
               type="num"
               value={(currentPage - 1) * limit + index + 1}
             />
             <TableBodyValue value={branch.outlet_name} />
             <TableBodyValue value={branch.outlet_address} />
+            <TableBodyValue value={branch.mainregion} />
             <TableBodyValue value={branch.outlet_region} />
-            <TableBodyValue value={branch.outlet_phone} position="end" />
+            <TableBodyValue value={branch.cctr_group} />
+            <TableBodyValue value={branch.cctr} />
+            <TableBodyValue value={branch.outlet_phone} />
+            <TableBodyValue value={branch.postcode} />
+            <TableBodyValue value={branch.coordinate} position="end" />
           </TableRow>
         ))}
       </TableData>
@@ -368,6 +530,124 @@ export const BranchList = ({ sectionId }) => {
               name="cctr"
               value={inputData.cctr}
               onChange={handleInputChange}
+              error={errors.cctr}
+            />
+          </InputWrapper>
+        </SubmitForm>
+      )}
+      {isEditOpen && (
+        <SubmitForm
+          formTitle="Edit Data Cabang"
+          onClose={closeEdit}
+          onSubmit={handleSubmitEdit}
+          saveText="Simpan Perubahan"
+          cancelText="Batal"
+          loading={isLoading}
+        >
+          <InputWrapper>
+            <UserInput
+              id="edit-outlet-name"
+              subVariant="label"
+              labelText="Nama Cabang"
+              placeholder="Edental Jakarta"
+              type="text"
+              name="name"
+              value={currentData.name}
+              onChange={handleInputEditChange}
+              error={errors.name}
+            />
+            <UserInput
+              id="edit-oultet-phone"
+              subVariant="label"
+              labelText="Nomor Kontak Cabang"
+              placeholder="0882xxx"
+              type="text"
+              name="phone"
+              value={currentData.phone}
+              onChange={handleInputEditChange}
+              error={errors.phone}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <UserInput
+              id="edit-outlet-mainregion"
+              subVariant="label"
+              labelText="Main Region"
+              placeholder="Jawa Barat"
+              type="text"
+              name="mainregion"
+              value={currentData.mainregion}
+              onChange={handleInputEditChange}
+              error={errors.mainregion}
+            />
+            <UserInput
+              id="edit-outlet-region"
+              subVariant="label"
+              labelText="Region"
+              placeholder="Bandung"
+              type="text"
+              name="region"
+              value={currentData.region}
+              onChange={handleInputEditChange}
+              error={errors.region}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <UserInput
+              id="edit-outlet-address"
+              subVariant="label"
+              labelText="Alamat Cabang"
+              placeholder="123 Main Street"
+              type="text"
+              name="address"
+              value={currentData.address}
+              onChange={handleInputEditChange}
+              error={errors.address}
+            />
+            <UserInput
+              id="edit-outlet-postcode"
+              subVariant="label"
+              labelText="Alamat Cabang"
+              placeholder="123 Main Street"
+              type="text"
+              name="postcode"
+              value={currentData.postcode}
+              onChange={handleInputEditChange}
+              error={errors.postcode}
+            />
+            <UserInput
+              id="edit-outlet-coordinate"
+              subVariant="label"
+              labelText="Titik Koordinat"
+              placeholder="Masukkan titik koordinat"
+              type="text"
+              name="coordinate"
+              value={currentData.coordinate}
+              onChange={handleInputEditChange}
+              error={errors.coordinate}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <UserInput
+              id="edit-outlet-cctrgroup"
+              subVariant="label"
+              labelText="CCTR Group"
+              placeholder="Masukkan CCTR Group"
+              type="text"
+              name="cctrGroup"
+              value={currentData.cctrGroup}
+              onChange={handleInputEditChange}
+              error={errors.cctrGroup}
+            />
+            <UserInput
+              id="edit-outlet-cctr"
+              subVariant="label"
+              labelText="CCTR"
+              placeholder="Masukkan CCTR"
+              type="text"
+              name="cctr"
+              value={currentData.cctr}
+              onChange={handleInputEditChange}
               error={errors.cctr}
             />
           </InputWrapper>
